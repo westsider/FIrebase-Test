a030faa9cb7bb3e8bbc150e45be3ae38a6dfbeab
Warren Hansen <whansen1@mac.com>
2017-09-26 17:29:00 -0700

    refactor: parse object for tableview

--- Firebase_Test/ViewController.swift
+++ Firebase_Test/ViewController.swift
@@ -28,12 +28,6 @@ import Firebase
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-//    var ref: DatabaseReference!
-//
-//    let currentChild = "-KupyHKs9UxNJiBn1iYO"
-//
-//    var userEmail = ""
-    
     let firebaseLink = FirebaseLink()
  
     var lastPriceList = [LastPrice]()
@@ -62,55 +56,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.detailTextLabel?.text = String(describing: lastPriceList[indexPath.row].close!)
         return cell
     }
-    
-//    func authFirebase() {
-//
-//        ref = Database.database().reference().child(currentChild).child("Table1")
-//        let email = "whansen1@mac.com"
-//        let password = "123456"
-//
-//        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
-//
-//            if error == nil {
-//                self.userEmail = (user?.email!)!
-//            } else {
-//                print(error ?? "something went wrong getting error")
-//            }
-//        }
-//    }
 
     func fetchValuesFromFireBase() {
     
-        //observing the data changes
         firebaseLink.ref.observe(DataEventType.value, with: { (snapshot) in
-            
-            //if the reference have some values
+   
             if snapshot.childrenCount > 0 {
                 
                 self.lastPriceList.removeAll()
-                
-                //iterating through all the values
-                for artists in snapshot.children.allObjects as! [DataSnapshot] {
-                    //getting values
-                    let artistObject = artists.value as? [String: AnyObject]
-               
-                    let date = DateHelper().convertToDateFrom(string: artistObject?["date"] as! String )
+     
+                for items in snapshot.children.allObjects as! [DataSnapshot] {
                     
-                    let open    = (artistObject?["open"] as! NSString).doubleValue
-
-                    let high    = (artistObject?["high"] as! NSString).doubleValue
-
-                    let low     = (artistObject?["low"] as! NSString).doubleValue
-
-                    let close   = (artistObject?["close"] as! NSString).doubleValue
-
-                    let lastPrice = LastPrice(ticker: "SPY", date: date, open: open, high: high, low: low, close: close, volume: 10000, signal: 0 )
-                    //  appending it to list
+                    let data    = items.value as? [String: AnyObject]
+                    let date    = DateHelper().convertToDateFrom(string: data?["date"] as! String )
+                    let open    = (data?["open"] as! NSString).doubleValue
+                    let high    = (data?["high"] as! NSString).doubleValue
+                    let low     = (data?["low"] as! NSString).doubleValue
+                    let close   = (data?["close"] as! NSString).doubleValue
+                    let lastPrice = LastPrice(ticker: "SPY", date: date, open: open,
+                                              high: high, low: low, close: close, volume: 10000, signal: 0 )
                     self.lastPriceList.append(lastPrice)
                 }
                 
                 self.lastPriceList = LastPriceTable().sortPrices(arrayToSort: self.lastPriceList)
-                //reloading the tableview
                 self.tableview.reloadData()
 
                 //for item in self.lastPriceList {
@@ -119,7 +87,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             }
         })
     }
-    
 }
 
 
