877fb0d6ed2de3ab8df71070b48282bc63054638
Warren Hansen <whansen1@mac.com>
2017-10-05 11:52:21 -0700

    refactor: firebase network call in main UI

--- Firebase_Test/FireBaseLink.swift
+++ Firebase_Test/FireBaseLink.swift
@@ -16,6 +16,12 @@ class FirebaseLink {
     
     var userEmail = ""
     
+    var lastPriceList = [LastPrice]()
+    
+    var currentLongEntryPrice:Double?
+    
+    var currentShortEntryPrice:Double?
+    
     func authFirebase() {
         
         ref = Database.database().reference()//.child(currentChild) //.child("Table1")
@@ -31,5 +37,74 @@ class FirebaseLink {
                 print(error ?? "something went wrong getting error")
             }
         }
+    }
+    
+    func fetchData(debug: Bool, dataComplete: @escaping (Bool) -> Void) {
+        
+        ref.observe(DataEventType.value, with: { (snapshot) in
+            
+            if snapshot.childrenCount > 0 {
+                
+                self.lastPriceList.removeAll()
+                
+                for items in snapshot.children.allObjects as! [DataSnapshot] {
+                    
+                    // get all other values ticker ect
+                    let data    = items.value as? [String: AnyObject]
+                    
+                    let ticker  = data?["ticker"] as! String
+                    let date    = DateHelper().convertToDateFrom(string: data?["date"] as! String )
+                    let open    = data?["open"] as! Double
+                    let high    = data?["high"] as! Double
+                    let low     = data?["low"] as! Double
+                    let close   = data?["close"] as! Double
+                    
+                    let signal  = data?["signal"] as! Double
+                    
+                    let trade   = data?["trade"] as! Double
+                    let bartype = data?["bartype"] as! String
+                    
+                    let connectStatus = data?["connectStatus"] as! String
+                    let connectTime = data?["connectTime"] as! String
+                    
+                    let longEntryPrice = data?["longEntryPrice"] as! Double
+                    let shortEntryPrice = data?["shortEntryPrice"] as! Double
+                    
+                    if (trade == 1) {
+                        //print("Long Entry")
+                        self.currentLongEntryPrice = longEntryPrice
+                    }
+                    if (trade == -1) {
+                        // print("Short Entry")
+                        self.currentShortEntryPrice = shortEntryPrice
+                    }
+                    
+                    let longLineLength = data?["longLineLength"] as! Int
+                    let shortLineLength = data?["shortLineLength"] as! Int
+                    let currentBar = data?["currentBar"] as! Int
+                    
+                    let inLong = data?["inLong"] as! Bool  // = (boolValue as! CFBoolean) as Bool
+                    let inShort = data?["inShort"] as! Bool
+                    
+                    let lastPrice = LastPrice(ticker: ticker, date: date, open: open ,
+                                              high: high, low: low, close: close, volume: 10000,
+                                              signal: signal, trade: trade, bartype: bartype,
+                                              connectStatus: connectStatus, connectTime: connectTime,
+                                              longEntryPrice: longEntryPrice, shortEntryPrice: shortEntryPrice,
+                                              longLineLength: longLineLength, shortLineLength: shortLineLength,
+                                              currentBar: currentBar, inLong: inLong, inShort: inShort)
+                    self.lastPriceList.append(lastPrice)
+                }
+                
+                if(debug) {
+                    for item in self.lastPriceList {
+                        print(item.date!, item.ticker!, item.open!, item.high!, item.low!, item.close!,
+                              item.signal!, item.trade!, item.bartype!, item.connectStatus!, item.connectTime!, item.longEntryPrice!, item.shortEntryPrice!, item.longLineLength!, item.shortLineLength!, item.currentBar!, item.inLong!, item.inShort!)
+                       // self.counter = self.counter + 1
+                    }
+                }
+                dataComplete(true) // i put this after the network call and the real write
+            }
+        })
     }
 }
--- Firebase_Test/MainViewController.swift
+++ Firebase_Test/MainViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 
 class MainViewController: UIViewController {
     
@@ -27,33 +26,29 @@ class MainViewController: UIViewController {
     
     let firebaseLink = FirebaseLink()
     
-    var lastPriceList = [LastPrice]()
-    
-    var json:[String:AnyObject]?
-    
-    var counter = 0;
-    
-    var currentLongEntryPrice:Double?
-    
-    var currentShortEntryPrice:Double?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "Trade Server"
         firebaseLink.authFirebase()
-        fetchValuesFromFireBase(debug: false)
+  
+        firebaseLink.fetchData(debug: false) { ( urlCallDone ) in
+            if urlCallDone {
+                print("network call finished")
+                self.updateUI()
+            }
+        }
     }
     
     func updateUI() {
         
-        let lastUpdate = lastPriceList.last
+        let lastUpdate = firebaseLink.lastPriceList.last
         
         var thisClose = 00.00
         
         //print("inLong: \(String(describing: lastUpdate?.inLong)) inShort: \(String(describing: lastUpdate?.inLong)) ")
 
-        if let inLong = lastUpdate?.inLong, let inShort = lastUpdate?.inShort, let longE:Double = currentLongEntryPrice, let shortE:Double = currentShortEntryPrice, let close = lastUpdate?.close {
+        if let inLong = lastUpdate?.inLong, let inShort = lastUpdate?.inShort, let longE:Double = firebaseLink.currentLongEntryPrice, let shortE:Double = firebaseLink.currentShortEntryPrice, let close = lastUpdate?.close {
 
             var lastPriceUpdateTop:String?
             
@@ -98,9 +93,9 @@ class MainViewController: UIViewController {
         }
         
 
-        let index = lastPriceList.count
+        let index = firebaseLink.lastPriceList.count
         if (index > 3) {
-            let priorBar = lastPriceList[index-2]
+            let priorBar = firebaseLink.lastPriceList[index-2]
             
             if let priorClose = priorBar.close {
                 let priceDiff = thisClose - priorClose
@@ -127,76 +122,6 @@ class MainViewController: UIViewController {
             let min = arr[1]
             serverConnectTime?.text = "\(hour!)" + ":" + min
         }
-    }
-
-    func fetchValuesFromFireBase(debug: Bool) {
-        
-        firebaseLink.ref.observe(DataEventType.value, with: { (snapshot) in
-            
-            if snapshot.childrenCount > 0 {
-                
-                self.lastPriceList.removeAll()
-                
-                for items in snapshot.children.allObjects as! [DataSnapshot] {
-                    
-                    // get all other values ticker ect
-                    let data    = items.value as? [String: AnyObject]
-                    
-                    let ticker  = data?["ticker"] as! String
-                    let date    = DateHelper().convertToDateFrom(string: data?["date"] as! String )
-                    let open    = data?["open"] as! Double
-                    let high    = data?["high"] as! Double
-                    let low     = data?["low"] as! Double
-                    let close   = data?["close"] as! Double
-                    
-                    let signal  = data?["signal"] as! Double
-                    
-                    let trade   = data?["trade"] as! Double
-                    let bartype = data?["bartype"] as! String
-                    
-                    let connectStatus = data?["connectStatus"] as! String
-                    let connectTime = data?["connectTime"] as! String
-                    
-                    let longEntryPrice = data?["longEntryPrice"] as! Double
-                    let shortEntryPrice = data?["shortEntryPrice"] as! Double
-                    
-                    if (trade == 1) {
-                        //print("Long Entry")
-                        self.currentLongEntryPrice = longEntryPrice
-                    }
-                    if (trade == -1) {
-                        // print("Short Entry")
-                        self.currentShortEntryPrice = shortEntryPrice
-                    }
-                    
-                    let longLineLength = data?["longLineLength"] as! Int
-                    let shortLineLength = data?["shortLineLength"] as! Int
-                    let currentBar = data?["currentBar"] as! Int
-                    
-                    let inLong = data?["inLong"] as! Bool  // = (boolValue as! CFBoolean) as Bool
-                    let inShort = data?["inShort"] as! Bool
-
-                    let lastPrice = LastPrice(ticker: ticker, date: date, open: open ,
-                                              high: high, low: low, close: close, volume: 10000,
-                                              signal: signal, trade: trade, bartype: bartype,
-                                              connectStatus: connectStatus, connectTime: connectTime,
-                                              longEntryPrice: longEntryPrice, shortEntryPrice: shortEntryPrice,
-                                              longLineLength: longLineLength, shortLineLength: shortLineLength,
-                                              currentBar: currentBar, inLong: inLong, inShort: inShort)
-                    self.lastPriceList.append(lastPrice)
-                }
-      
-                if(debug) {
-                    for item in self.lastPriceList {
-                        print(item.date!, item.ticker!, item.open!, item.high!, item.low!, item.close!,
-                              item.signal!, item.trade!, item.bartype!, item.connectStatus!, item.connectTime!, item.longEntryPrice!, item.shortEntryPrice!, item.longLineLength!, item.shortLineLength!, item.currentBar!, item.inLong!, item.inShort!)
-                        self.counter = self.counter + 1
-                    }
-                }
-                
-                self.updateUI()
-            }
-        })
     }
    
 }
